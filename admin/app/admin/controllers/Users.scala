311ff0aec2e482450acc17e00cf03c2eeacc53a3
dungvn3000@gmail.com <ducdung2512>
2014-01-07 16:02:35 +0700

    Init Login Page

--- admin/app/admin/controllers/Users.scala
+++ admin/app/admin/controllers/Users.scala
@@ -3,6 +3,7 @@ package admin.controllers
 import play.api.mvc._
 import jp.t2v.lab.play2.auth.AuthElement
 import auth.AuthConfigImpl
+import model.Administrator
 
 /**
  * The Class Users.
@@ -13,8 +14,8 @@ import auth.AuthConfigImpl
  */
 object Users extends Controller with AuthElement with AuthConfigImpl with AdminTemplate {
 
-  def index = Action {
+  def index = StackAction(AuthorityKey -> Administrator)(implicit request => {
     renderOk(admin.views.html.users.list())
-  }
+  })
 
 }
