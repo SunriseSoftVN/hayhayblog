933f52d3e1e8d25aa5d99320624916666b780ead
dungvn3000@gmail.com <ducdung2512>
2014-01-07 15:28:18 +0700

    Init Login Page

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -6,7 +6,7 @@ import auth.AuthConfigImpl
 import play.api.data.Form
 import play.api.data.Forms._
 import dao.UserDao
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 import ExecutionContext.Implicits.global
 
 /**
@@ -27,11 +27,26 @@ object Users extends Controller with AuthElement with AuthConfigImpl with LoginL
     })
   }
 
-  def login = Action {
-    Ok(views.html.users.login())
+  def recover = Action {
+    Ok(views.html.users.recoverpassword(loginForm))
   }
 
-  def auth = TODO
+  def register = Action {
+    Ok(views.html.users.register(loginForm))
+  }
+
+  def login = Action {
+    Ok(views.html.users.login(loginForm))
+  }
+
+  def auth = Action.async(implicit request => {
+    loginForm.bindFromRequest.fold(
+      error => Future.successful(BadRequest(views.html.users.login(error))),
+      form => {
+        gotoLoginSucceeded(form._1)
+      }
+    )
+  })
 
   def logout = Action.async(implicit request => {
     gotoLogoutSucceeded
