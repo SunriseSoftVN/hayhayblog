2c27df4852025ebdeedf7dc2c25d40d611902bd8
dungvn3000@gmail.com <ducdung2512>
2014-01-08 10:30:25 +0700

    Implement Category page

--- admin/app/admin/controllers/Categories.scala
+++ admin/app/admin/controllers/Categories.scala
@@ -3,9 +3,13 @@ package admin.controllers
 import play.api.mvc._
 import jp.t2v.lab.play2.auth.AuthElement
 import auth.AuthConfigImpl
-import model.Administrator
+import model.{Category, Administrator}
 import dao.CategoryDao
 import utils.String2Int
+import play.api.data.Form
+import play.api.data.Forms._
+import com.mongodb.casbah.commons.Imports._
+import formater.ObjectIdFormat._
 
 /**
  * The Class Categories.
@@ -18,6 +22,12 @@ object Categories extends Controller with AuthElement with AuthConfigImpl with A
 
   lazy val query_field = "name"
   lazy val sort_field = "name"
+  lazy val form = Form(
+    mapping(
+      "_id" -> default(of[ObjectId], new ObjectId()),
+      "name" -> nonEmptyText(minLength = 3)
+    )(Category.apply)(Category.unapply)
+  )
 
   def index = StackAction(AuthorityKey -> Administrator)(implicit request => {
     val page = request.getQueryString("page").collect {
@@ -39,6 +49,10 @@ object Categories extends Controller with AuthElement with AuthConfigImpl with A
     )
 
     renderOk(admin.views.html.categories.list(users, totalPage, page, query))
+  })
+
+  def create = StackAction(AuthorityKey -> Administrator)(implicit request => {
+    renderOk(admin.views.html.categories.edit(form))
   })
 
 }
---
+++ core/app/formater/ObjectIdFormat.scala
@@ -0,0 +1,62 @@
+package formater
+
+import play.api.data.format.Formatter
+import org.bson.types.ObjectId
+import play.api.libs.json._
+import play.api.libs.json.JsSuccess
+import play.api.data.FormError
+import play.api.libs.json.JsString
+import scala.Some
+
+/**
+ * The Class Format.
+ *
+ * @author Nguyen Duc Dung
+ * @since 12/31/12 2:04 PM
+ *
+ */
+object ObjectIdFormat {
+
+  /**
+   * For a form
+   * @return
+   */
+  implicit def objectIdFormatter: Formatter[ObjectId] = new Formatter[ObjectId] {
+    override val format = Some("format.objectid", Nil)
+
+    def unbind(key: String, value: ObjectId) = Map(key -> value.toString)
+
+    def bind(key: String, data: Map[String, String]): Either[Seq[FormError], ObjectId] = data.get(key).map(value => {
+      if(ObjectId.isValid(value)) {
+        Right(new ObjectId(value))
+      } else if(value.isEmpty) {
+        Right(new ObjectId())
+      } else {
+        Left(Seq(FormError(key, "error.id", Nil)))
+      }
+    }).getOrElse(
+      Right(new ObjectId())
+    )
+  }
+
+
+  /**
+   * For json
+   */
+  implicit val objectIdFormat: Format[ObjectId] = new Format[ObjectId] {
+    def reads(json: JsValue) = {
+      json match {
+        case jsString: JsString => {
+          if (ObjectId.isValid(jsString.value)) JsSuccess(new ObjectId(jsString.value))
+          else JsError("Invalid ObjectId")
+        }
+        case other => JsError("Can't parse json path as an ObjectId. Json content = " + other.toString())
+      }
+    }
+
+    def writes(oId: ObjectId): JsValue = {
+      JsString(oId.toString)
+    }
+  }
+
+}
