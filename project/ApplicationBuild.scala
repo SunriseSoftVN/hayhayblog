4aaf68fe774f64fb21d02b79c865859be1bf4701
dungvn3000@gmail.com <ducdung2512>
2014-01-29 15:19:50 +0700

    Update

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -7,10 +7,21 @@ object ApplicationBuild extends Build {
   val appName = "hayhayblog"
   val appVersion = "0.0.1"
 
-  val appDependencies = Seq(
+  val templateEngine = Seq(
+    "com.mohiva" %% "play-html-compressor" % "0.2-SNAPSHOT",
+    "org.ocpsoft.prettytime" % "prettytime" % "3.2.4.Final",
+    "com.scalatags" %% "scalatags" % "0.2.2"
+  )
+
+  val persistentDependencies = Seq(
     "joda-time" % "joda-time" % "2.1",
     "org.joda" % "joda-convert" % "1.2",
     "org.mongodb" % "mongo-java-driver" % "2.11.2",
+    "org.joda" % "joda-money" % "0.9",
+    "se.radley" %% "play-plugins-salat" % "1.4.0" exclude("joda-time", "joda-time")
+  )
+
+  val appDependencies = Seq(
     "commons-collections" % "commons-collections" % "3.2.1",
     "org.apache.commons" % "commons-lang3" % "3.1",
     "org.apache.commons" % "commons-math3" % "3.0",
@@ -18,19 +29,16 @@ object ApplicationBuild extends Build {
     "commons-validator" % "commons-validator" % "1.4.0" exclude("commons-beanutils", "commons-beanutils"),
     "commons-io" % "commons-io" % "2.4",
     "org.apache.httpcomponents" % "httpclient" % "4.2.5",
-    "se.radley" %% "play-plugins-salat" % "1.4.0" exclude("joda-time", "joda-time"),
     "jp.t2v" %% "stackable-controller" % "0.3.0",
     "jp.t2v" %% "play2-auth" % "0.11.0",
     "jp.t2v" %% "play2-auth-test" % "0.11.0" % "test",
-    "org.jsoup" % "jsoup" % "1.7.2",
+    "org.jsoup" % "jsoup" % "1.7.3",
     "org.scalanlp" %% "breeze-process" % "0.3",
     "org.apache.commons" % "commons-email" % "1.3.1",
-    "org.ocpsoft.prettytime" % "prettytime" % "3.1.0.Final",
-    "com.mohiva" %% "play-html-compressor" % "0.2-SNAPSHOT",
     "com.restfb" % "restfb" % "1.6.11",
     "com.google.http-client" % "google-http-client-jackson2" % "1.11.0-beta",
     "com.google.apis" % "google-api-services-oauth2" % "v2-rev48-1.16.0-rc"
-  )
+  ) ++ persistentDependencies ++ templateEngine
 
   val appResolvers = Seq(
     "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
