b150ba8a03bb55d7dab5d7bb5cb10036c97901bd
dungvn3000@gmail.com <ducdung2512>
2014-02-02 21:48:04 +0700

    Update

--- app/controllers/MainTemplate.scala
+++ app/controllers/MainTemplate.scala
@@ -15,19 +15,22 @@ import dao.CategoryDao
  */
 trait MainTemplate extends Controller {
 
+  def rightSideBar = views.html.partials.rightsidebar()
 
-  def navbar(user: Option[User]) = views.html.partials.navbar(user, CategoryDao.all)
+  def navbar = views.html.partials.topmenu(CategoryDao.all)
+
+  def footer = views.html.partials.footer(CategoryDao.all)
 
   def renderOk(content: Html,
                title: String = Messages("application.title"),
                description: String = Messages("application.description"))(implicit user: Option[User]) = Ok(
-    views.html.tml.main(content, title, description, navbar(user))
+    views.html.tml.main(content, title, description, navbar, footer, rightSideBar)
   )
 
   def renderBadRequest(content: Html,
                        title: String = Messages("application.title"),
                        description: String = Messages("application.description"))(implicit user: Option[User]) = BadRequest(
-    views.html.tml.main(content, title, description, navbar(user))
+    views.html.tml.main(content, title, description, navbar, footer, rightSideBar)
   )
 
   implicit class RichOption[E](op: Option[E]) {
