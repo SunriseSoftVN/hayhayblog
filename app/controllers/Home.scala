7273936fceda87c2262e36d5b2cdf023a31f70b9
dungvn3000@gmail.com <ducdung2512>
2014-02-04 15:04:34 +0700

    Update new ui

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -10,7 +10,7 @@ object Home extends Controller with OptionalAuthElement with AuthConfigImpl with
 
   def index = StackAction(implicit request => {
     val newsBoxs = CategoryDao.all.map(cat => {
-      val (articles, totalPage) = ArticleDao.findByCatId(cat._id, page = 1, itemDisplay = 3)
+      val (articles, totalPage) = ArticleDao.findByCatId(cat._id, page = 1, itemDisplay = 5)
       NewsBoxDto(cat, articles)
     })
 
