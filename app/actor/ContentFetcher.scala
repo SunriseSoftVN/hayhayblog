8618c2412605625be4fd01740133b1e09ef0189d
dungvn3000@gmail.com <ducdung2512>
2014-02-24 16:40:06 +0700

    Update.

--- app/actor/ContentFetcher.scala
+++ app/actor/ContentFetcher.scala
@@ -2,14 +2,7 @@ package actor
 
 import akka.actor.{ActorRef, Props, Actor}
 import model.Article
-import play.api.Logger
 import org.apache.http.client.HttpClient
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.HttpStatus
-import org.apache.http.util.EntityUtils
-import parser.HtmlExtractor
-import org.jsoup.Jsoup
-import java.io.ByteArrayInputStream
 import play.api.libs.concurrent.Akka
 import akka.routing.RoundRobinRouter
 import play.api.Play.current
@@ -28,23 +21,25 @@ class ContentFetcher(httpClient: HttpClient, persistent: ActorRef) extends Actor
 
   override def receive = {
     case article: Article =>
-      try {
-        val response = httpClient.execute(new HttpGet(article.url))
-        Logger.info(s"Download ${response.getStatusLine.getStatusCode} : ${article.url}")
-        if (response.getStatusLine.getStatusCode == HttpStatus.SC_OK) {
-          val entity = response.getEntity
-          val content = EntityUtils.toByteArray(entity)
-          val htmlExtractor = new HtmlExtractor
-          val input = new ByteArrayInputStream(content)
-          val doc = Jsoup.parse(input, null, article.url)
-          htmlExtractor.extract(doc)
-          article.potentialImages ++= htmlExtractor.images.toList
-          imageFetcher ! article
-        }
-        EntityUtils.consume(response.getEntity)
-      } catch {
-        case ex: Exception =>
-          Logger.error("Error: " + article.url, ex)
-      }
+      imageFetcher ! article
+
+//      try {
+//        val response = httpClient.execute(new HttpGet(article.url))
+//        Logger.info(s"Download ${response.getStatusLine.getStatusCode} : ${article.url}")
+//        if (response.getStatusLine.getStatusCode == HttpStatus.SC_OK) {
+//          val entity = response.getEntity
+//          val content = EntityUtils.toByteArray(entity)
+//          val htmlExtractor = new HtmlExtractor
+//          val input = new ByteArrayInputStream(content)
+//          val doc = Jsoup.parse(input, null, article.url)
+//          htmlExtractor.extract(doc)
+//          article.potentialImages ++= htmlExtractor.images.toList
+//          imageFetcher ! article
+//        }
+//        EntityUtils.consume(response.getEntity)
+//      } catch {
+//        case ex: Exception =>
+//          Logger.error("Error: " + article.url, ex)
+//      }
   }
 }
