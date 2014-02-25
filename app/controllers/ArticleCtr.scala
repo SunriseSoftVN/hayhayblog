7fe90085732f4ffccd183e4cc373e98fb018cb8e
dungvn3000@gmail.com <ducdung2512>
2014-02-25 13:08:59 +0700

    Revamp hayhayblog focus on main idea

--- app/controllers/ArticleCtr.scala
+++ app/controllers/ArticleCtr.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api.mvc.Controller
 import jp.t2v.lab.play2.auth.OptionalAuthElement
 import auth.AuthConfigImpl
-import dao.{TagDao, BlogDao, ArticleDao, CategoryDao}
+import dao.{BlogDao, ArticleDao, CategoryDao}
 import dto.TopMenuDto
 
 /**
@@ -18,32 +18,24 @@ object ArticleCtr extends Controller with OptionalAuthElement with AuthConfigImp
   def index(shortName: String, page: Int) = StackAction(implicit request => {
     CategoryDao.findByShortName(shortName).mapRender(cat => {
       val (articles, totalPage) = ArticleDao.findByCatId(cat._id, page)
-      val articlesNextPage = if (page < totalPage) ArticleDao.findByCatId(cat._id, page + 1)._1 else Nil
       implicit val topMenuDto = TopMenuDto(CategoryDao.all, cat.shortName)
-      val blogs = BlogDao.findByCatId(cat._id)
-      val tags = TagDao.top
-      renderOk(views.html.article.view(articles, ArticleDao.mostReadByCatId(cat._id, 5), articlesNextPage, blogs, tags, totalPage, page))
+      renderOk(views.html.article.view(articles, totalPage, page))
     })
   })
 
   def blog(blogName: String, page: Int) = StackAction(implicit request => {
     BlogDao.findByBlogName(blogName).mapRender(blog => {
       val (articles, totalPage) = ArticleDao.findByBlogId(blog._id, page)
-      val articlesNextPage = if (page < totalPage) ArticleDao.findByBlogId(blog._id, page + 1)._1 else Nil
       val catName = blog.category.map(_.shortName).getOrElse("")
       implicit val topMenuDto = TopMenuDto(CategoryDao.all, catName)
-      val blogs = BlogDao.findByCatId(blog.categoryId).filterNot(_._id == blog._id)
-      val tags = TagDao.top
-      renderOk(views.html.article.view(articles, ArticleDao.mostReadByBlogId(blog._id, 5), articlesNextPage, blogs, tags, totalPage, page))
+      renderOk(views.html.article.view(articles, totalPage, page))
     })
   })
 
   def tag(tagName: String, page: Int) = StackAction(implicit request => {
     val (articles, totalPage) = ArticleDao.findByTag(tagName.replaceAll("_", ".*"), page)
-    val articlesNextPage = if (page < totalPage) ArticleDao.findByTag(tagName, page + 1)._1 else Nil
     implicit val topMenuDto = TopMenuDto(CategoryDao.all, "none")
-    val tags = TagDao.top
-    renderOk(views.html.article.view(articles, Nil, articlesNextPage, Nil, tags, totalPage, page))
+    renderOk(views.html.article.view(articles, totalPage, page))
   })
 
 }
--- app/controllers/HomeCtr.scala
+++ app/controllers/HomeCtr.scala
@@ -3,20 +3,12 @@ package controllers
 import play.api.mvc._
 import jp.t2v.lab.play2.auth.OptionalAuthElement
 import auth.AuthConfigImpl
-import dao.{BlogDao, ArticleDao, CategoryDao}
-import dto.NewsBoxDto
+import dao.ArticleDao
 
 object HomeCtr extends Controller with OptionalAuthElement with AuthConfigImpl with MainTemplate {
 
   def index = StackAction(implicit request => {
-    val newsBoxs = CategoryDao.all.map(cat => {
-      val (articles, totalPage) = ArticleDao.findByCatId(cat._id, page = 1, itemDisplay = 5)
-      val blogs = BlogDao.findByCatId(cat._id)
-      NewsBoxDto(cat, blogs, articles)
-    })
-
-    val blogs = BlogDao.top
-    renderOk(views.html.index(newsBoxs, ArticleDao.mostRead(6), ArticleDao.latest(20), blogs))
+    renderOk(views.html.index(ArticleDao.latest(20)))
   })
 
 }
