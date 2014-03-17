6c22a4385089f2f19239857992a9e203d0c85bef
dungvn3000@gmail.com <ducdung2512>
2014-03-17 07:33:38 +0700

    Update some seo improvement.

--- app/controllers/ArticleCtr.scala
+++ app/controllers/ArticleCtr.scala
@@ -19,7 +19,7 @@ object ArticleCtr extends Controller with OptionalAuthElement with AuthConfigImp
     CategoryDao.findByShortName(shortName).mapRender(cat => {
       val (articles, totalPage) = ArticleDao.findByCatId(cat._id, page, 20)
       implicit val topMenuDto = TopMenuDto(CategoryDao.all, cat.shortName)
-      renderOk(views.html.article.view(articles, totalPage))
+      renderOk(views.html.article.view(articles, totalPage), title = "Các blog hay về " + cat.name.toLowerCase)
     })
   })
 
@@ -28,14 +28,14 @@ object ArticleCtr extends Controller with OptionalAuthElement with AuthConfigImp
       val (articles, totalPage) = ArticleDao.findByBlogId(blog._id, page, 20)
       val catName = blog.category.map(_.shortName).getOrElse("")
       implicit val topMenuDto = TopMenuDto(CategoryDao.all, catName)
-      renderOk(views.html.article.view(articles, totalPage))
+      renderOk(views.html.article.view(articles, totalPage), title = "Blog " + blog.name)
     })
   })
 
   def tag(tagName: String, page: Int) = StackAction(implicit request => {
     val (articles, totalPage) = ArticleDao.findByTag(tagName.replaceAll("_", ".*"), page, 20)
     implicit val topMenuDto = TopMenuDto(CategoryDao.all, "none")
-    renderOk(views.html.article.view(articles, totalPage))
+    renderOk(views.html.article.view(articles, totalPage), title = "Các bài viết hay về " + tagName.replaceAll("_", " "))
   })
 
 }
